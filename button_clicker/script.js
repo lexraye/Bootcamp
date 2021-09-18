405e24d05763372409022c0874d20bff91e4dffc
Alexa Sigwing <70794561+lexraye@users.noreply.github.com>
2021-09-17 17:09:36 -0700

    Add files via upload

---
+++ button_clicker/script.js
@@ -0,0 +1,14 @@
+//This will change the login button to say sign out when clicked
+function logOut(login){
+  login.innerText = "Sign Out";
+}
+
+//This will create an alert when like button is clicked
+function likeButton(likes){
+  alert("Ninja was liked");
+}
+
+//This will remove add definition button when clicked
+function hide(add){
+  add.remove();
+}

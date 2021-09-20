9230fbb5d3adbb1c4a614d9b0e46e7f52a3f3972
Alexa Sigwing <70794561+lexraye@users.noreply.github.com>
2021-09-19 22:32:51 -0700

    Add files via upload

---
+++ like_buttons/part_2/script.js
@@ -0,0 +1,32 @@
+var like1 = 9;
+var likeElement1 = document.querySelector(".amount-one");
+
+console.log(likeElement1);
+
+function buttonClick1() {
+  like1 ++;
+  likeElement1.innerHTML = like1;
+  console.log(like1);
+}
+
+var like2 = 12;
+var likeElement2 = document.querySelector(".amount-two");
+
+console.log(likeElement2);
+
+function buttonClick2() {
+  like2 ++;
+  likeElement2.innerHTML = like2;
+  console.log(like2);
+}
+
+var like3 = 9;
+var likeElement3 = document.querySelector(".amount-three");
+
+console.log(likeElement3);
+
+function buttonClick3() {
+  like3 ++;
+  likeElement3.innerHTML = like3;
+  console.log(like3);
+}
